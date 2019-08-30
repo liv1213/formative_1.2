3c103025cd08b5b80d11f830dcf7841551d4c8cf
Olivia Lonsdale <olivialonsdale@MacBook-Pro-2.local>
2019-08-30 16:00:09 +1200

    implimented nav drop down

--- js/app.js
+++ js/app.js
@@ -7,3 +7,14 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
+// Overlay menu
+
+let overlayVineyards= document.querySelector('.overlay-vinyards')
+let overlayDropdown = document.querySelector('.overlay-drop-down')
+
+overlayVineyards.addEventListener('click', function(evt){
+overlayDropdown.classList.toggle('slide-down')
+})
+
+
+
